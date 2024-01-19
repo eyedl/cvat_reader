0ea7c94553117cc5006ed7c83544a48f99491a63
Jelmer Wilhelm <jelmerwilhelm@gmail.com>
2024-01-19 15:02:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def setup_package():
 
     setup(
         name="cvat_reader",
-        version="0.3.2",
+        version="0.3.3",
         author="Koen Vossen",
         author_email="info@koenvossen.nl",
         url="https://github.com/eyedl/cvat_reader",
